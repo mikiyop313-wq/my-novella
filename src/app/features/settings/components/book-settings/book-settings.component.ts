37cb0032f5a5c1bdd39b922eb9c2521f8ad5726c
mikiyop313-wq <mikiyop313@gmail.com>
2026-08-16 00:06:40 +0300

    feat(settings): scaffold global and book settings components

---
+++ src/app/features/settings/components/book-settings/book-settings.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-settings',
+  templateUrl: './book-settings.component.html',
+  styleUrl: '../../styles/settings.shared.scss',
+})
+export class BookSettingsComponent {}
---
+++ src/app/features/settings/components/global-settings/global-settings.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-settings',
+  templateUrl: './global-settings.component.html',
+  styleUrl: '../../styles/settings.shared.scss',
+})
+export class GlobalSettingsComponent {}
