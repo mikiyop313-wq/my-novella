27ec6c416322af2b8d96615a74d75f99a378bb56
mikiyop313-wq <mikiyop313@gmail.com>
2026-08-15 23:58:31 +0300

    feat: implement book modal component and overlay directive with supporting styles and icons

--- src/app/features/library/components/book-card/book-card.component.ts
+++ src/app/features/library/components/book-card/book-card.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { OverlayMenuDirective } from '../../../../shared/directives/overlay-menu.directive';
+import { OverlayModalDirective } from '../../../../shared/directives/overlay-modal.directive';
+import { BookModalComponent } from '../book-modal/book-modal.component';
 
 @Component({
   selector: 'app-book-card',
-  imports: [OverlayMenuDirective],
+  imports: [OverlayMenuDirective, OverlayModalDirective, BookModalComponent],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.scss',
 })
